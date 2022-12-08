148b995d38aeacac1621d68526842aab65652d41
aderibigbeganiu <aderibigbeganiu@gmail.com>
2022-12-08 21:07:30 +0100

    working gateway

--- auth_svc/access.py
+++ auth_svc/access.py
@@ -1,5 +1,8 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def login(request):
@@ -8,11 +11,12 @@ def login(request):
         return None, ("Missing credentials", 401)
     basicAuth = auth.username, auth.password
 
+
     response = requests.post(
-        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}", auth=basicAuth
+        f'{os.environ.get("AUTH_SVC_ADDRESS")}/login', auth=basicAuth
     )
 
     if response.status_code == 200:
-        return response.text
+        return response.text, None
     else:
         return None, (response.text, response.status_code)
